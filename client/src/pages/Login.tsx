2fd764cb75c4132c74724a1a5b7aa9e3289c94b7
jctaoo <jctaoo@outlook.com>
2021-08-17 14:04:18 +0000

    feat(client): create login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,103 @@
+import {
+  HStack,
+  Img,
+  VStack,
+  Text,
+  InputGroup,
+  Input,
+  useBoolean,
+  InputRightElement,
+  Button,
+  Container,
+  Flex,
+  IconButton,
+} from "@chakra-ui/react";
 import React from "react";
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+import posterImage from "../assets/poster.png";
 
 function LoginPage() {
+  const [showPass, { toggle: toggleShowPass }] = useBoolean(false);
+
   return (
-    <div id="admin">
-      <h1>Login</h1>
-    </div>
+    <Container
+      as="div"
+      id="login-page"
+      maxW="container.lg"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <HStack>
+        <Img
+          src={posterImage}
+          height="100vh"
+          objectFit="contain"
+          flex="3"
+          display={["none", "none", "block"]}
+        />
+        <Flex flex="5" alignItems="center" justifyContent="center">
+          <VStack alignItems="start" spacing="30px" display="inline-flex">
+            <Text
+              fontWeight="black"
+              fontSize="40"
+              color="brandPrimary"
+              lineHeight="3rem"
+            >
+              Welcome <br />
+              Codeuniverse
+            </Text>
+            <VStack spacing="3">
+              <Input
+                placeholder="Email or Username"
+                borderColor="brandPrimary"
+                size="lg"
+              ></Input>
+              <VStack alignItems="start">
+                <InputGroup size="lg">
+                  <Input
+                    pr="4.5rem"
+                    type={showPass ? "text" : "password"}
+                    placeholder="Enter password"
+                    borderColor="brandPrimary"
+                    size="lg"
+                  />
+                  <InputRightElement>
+                    <IconButton
+                      aria-label="Toggle password visibility"
+                      variant="ghost"
+                      size="sm"
+                      onClick={toggleShowPass}
+                      colorScheme="brandScheme"
+                      icon={
+                        showPass ? <AiOutlineEye /> : <AiOutlineEyeInvisible />
+                      }
+                    />
+                  </InputRightElement>
+                </InputGroup>
+                <Button variant="link" colorScheme="brandScheme">
+                  Forget Passworld ?
+                </Button>
+              </VStack>
+            </VStack>
+            <HStack spacing="5">
+              <Button colorScheme="brandScheme" size="lg">
+                Log in
+              </Button>
+              <Button
+                colorScheme="brandScheme"
+                variant="outline"
+                borderWidth="2px"
+                size="lg"
+              >
+                Sign up
+              </Button>
+            </HStack>
+          </VStack>
+        </Flex>
+      </HStack>
+    </Container>
   );
 }
 
--- client/src/theme.ts
+++ client/src/theme.ts
@@ -2,6 +2,19 @@ import { extendTheme, theme as baseTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   colors: {
+    brandScheme: {
+      50: "#E7D8FA",
+      100: "#DFC7FD",
+      200: "#CFAFF6",
+      300: "#C69EF8",
+      400: "#BF90FA",
+      500: "#B981FF",
+      600: "#A460FA",
+      700: "#8B34F9",
+      800: "#7C1DF5",
+      900: "#7000FE",
+    },
+    brandPrimary: "#B981FF",
     primary: baseTheme.colors.pink[300],
     primaryHover: baseTheme.colors.pink[400],
     primarySelected: baseTheme.colors.pink[500],
